1074038e2d535c17ae4bcdb1bc89d5977734ae62
Dios <David@Daweis-MacBook-Pro.local>
2017-01-01 15:34:49 -0800

    Fetching data

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import  { connect } from 'react-redux';
+// import  { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-    return <div>List of blog post</div>
+class PostIndex extends Component {
+    
+    componentWillMount(){
+        this.props.fetchPosts();   
+    }
+    
+    render() {
+        return (
+            <div> List of blog post </div>
+
+        );
+    }
 }
+
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators({fetchPosts},dispatch);
+// }
+
+export default connect(null, {fetchPosts})(PostIndex);
